b856f650dc41fb25cfefcb482e58d6b675389036
Swyter <swyterzone@gmail.com>
2019-03-21 01:19:22 +0100

    Refactor everything into its own class, as recommended and add the skeleton of a dummy background asyncio task. Complete the BeautifulSoup4 Workshop scrapper, add a few safeguards.

--- tld-bot.py
+++ tld-bot.py
@@ -17,57 +17,88 @@ if not 'DISCORD_TOKEN' in os.environ:
   print('[!] Set your DISCORD_TOKEN environment variable to your Discord client secret.')
   sys.exit(-1)
 
-client = discord.Client()
+class TldDiscordClient(discord.Client):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
 
-@client.event
-async def on_message(message):
-  # we do not want the bot to reply to itself or web-hooks
-  if message.author == client.user or type(message.author) is not discord.Member or message.author.bot:
-    print("Bot:", pprint(message.author))
-    return
+    # create the background task and run it in the background
+    self.bg_task = self.loop.create_task(self.my_background_task())
 
-  print(pprint(message.author.roles))
-  if message.content.startswith('!hello') and any(x in str(message.author.roles) for x in ['Swyter', 'Developer', 'Volunteer']):
-    msg = 'Hello {0.author.mention}'.format(message)
-    await message.channel.send(msg)
-
-  if message.content.startswith('!rank'):
-    await message.add_reaction('🔨')
+  async def on_ready(self):
+    print('Logged in as')
+    print(self.user.name)
+    print(self.user.id)
+    print('------')
     
-  if message.content.startswith('!embed'):
-    embed = discord.Embed(colour=discord.Colour(0x1b2148), url="https://discordapp.com", description="Good news, we have deployed a new Workshop update.\nTake a look at our updated TLD [changelog here](https://steamcommunity.com/sharedfiles/filedetails/changelog/299974223#profileBlock).")
+    channel = self.get_channel(470890531061366787)
+    await channel.send("LOL")
+
+  async def on_message(self, message):
+    # swy: no weird commands here, boy!
+    if message.content.startswith('!rank'):
+      await message.add_reaction('🔨')
+
+    # swy: we do not want the bot to reply to itself or web-hooks
+    if message.author == self.user or type(message.author) is not discord.Member or message.author.bot:
+      print("Bot:", pprint(message.author))
+      return
+      
+    # swy: ignore messages from users that aren't in the whitelist
+    if not any(x in str(message.author.roles) for x in ['Swyter', 'Developer', 'Volunteer']):
+      print("Ignored:", pprint(message.author))
+      return
+
+    print(pprint(message.author.roles))
     
-    embed.set_thumbnail(url="https://avatars1.githubusercontent.com/u/12862724?s=400&u=223b3e00f52394fc6b5690999970a755f5444aab&v=4")
-    embed.set_author(name="New Steam Workshop update — 2019-20-23 @Swyter", url="https://steamcommunity.com/sharedfiles/filedetails/changelog/299974223#profileBlock", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/200px-Steam_icon_logo.svg.png")
-    
-    embed.add_field(name="➥ Restart your Steam client to force an update", value="Updates should be automatic, but they may take a few minutes.", inline=True)
-    embed.add_field(name="➥ How do I get this? I'm using the manual install", value="You need to own the game on Steam and [subscribe here](https://steamcommunity.com/sharedfiles/filedetails/?id=299974223#profileBlock).")
+    # swy: useful for testing
+    if message.content.startswith('!hello'):
+      msg = 'Hello {0.author.mention}'.format(message)
+      await message.channel.send(msg)
 
-    await message.channel.send(embed=embed)
+    # swy: same thing
+    if message.content.startswith('!embed'):
+      embed = discord.Embed(colour=discord.Colour(0x1b2148), url="https://discordapp.com", description="Good news, we have deployed a new Workshop update.\nTake a look at our updated TLD [changelog here](https://steamcommunity.com/sharedfiles/filedetails/changelog/299974223#profileBlock).")
+      
+      embed.set_thumbnail(url="https://avatars1.githubusercontent.com/u/12862724?s=400&u=223b3e00f52394fc6b5690999970a755f5444aab&v=4")
+      embed.set_author(name="New Steam Workshop update — 2019-20-23 @Swyter", url="https://steamcommunity.com/sharedfiles/filedetails/changelog/299974223#profileBlock", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/200px-Steam_icon_logo.svg.png")
+      
+      embed.add_field(name="➥ Restart your Steam client to force an update", value="Updates should be automatic, but they may take a few minutes.", inline=True)
+      embed.add_field(name="➥ How do I get this? I'm using the manual install", value="You need to own the game on Steam and [subscribe here](https://steamcommunity.com/sharedfiles/filedetails/?id=299974223#profileBlock).")
 
-@client.event
-async def on_ready():
-  print('Logged in as')
-  print(client.user.name)
-  print(client.user.id)
-  print('------')
+      await message.channel.send(embed=embed)
 
-@client.event
-async def on_member_join(member):
-  print(pprint(member))
-  print('User joined: ', member)
+  async def on_member_join(self, member):
+    print(pprint(member))
+    print('User joined: ', member)
 
-  if member.name in ['cpt', 'cp', 'sgt', 'tp'] and \
-     member.name == member.display_name and \
-     member.avatar == None and \
-     len(member.roles) <= 1:
-    client.ban(member, reason='[Automatic] Suspected bot or automated account.')
-    msg = 'Preemptively banned {0.mention}, probably some automated account. 🔨'.format(member)
-    await message.channel.send(msg)
+    if member.name in ['cpt', 'cp', 'sgt', 'tp'] and \
+       member.name == member.display_name and \
+       member.avatar == None and \
+       len(member.roles) <= 1:
+      self.ban(member, reason='[Automatic] Suspected bot or automated account.')
+      
+      # swy: send a channel to #off-topic
+      await self.get_channel("493040076511510550").send('Preemptively banned {0.mention}, probably some automated account. 🔨'.format(member))
 
-@client.event
-async def on_message_delete(message):
-  print('Deleted message:', pprint(message))
+  async def on_message_delete(self, message):
+    print('Deleted message:', pprint(message))
+
+  async def my_background_task(self):
+      print("my_background_task")
+      await self.wait_until_ready()
+      print("my_background_task2")
+      counter = 0
+      channel = self.get_channel(470890531061366787)
+      print("my_background_task3", self.is_closed())
+      
+      while not self.is_closed():
+          counter += 1
+          print("Sent recurrent message %u" % counter)
+          await channel.send(counter)
+          await asyncio.sleep(60) # task runs every 60 seconds
+
+
+client = TldDiscordClient()
 
 while True:
   try:
