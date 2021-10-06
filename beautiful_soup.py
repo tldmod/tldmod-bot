2f6f9bd5375f150da3cbeb66ec9dcbb65aaf896c
Swyter <swyterzone@gmail.com>
2021-10-06 02:23:24 +0200

    Refactor the Worshop scrapper to make it more resilient against generalized Valve downtime problems. Check a different/unrelated Workshop page and check if that one is also error'ing out; false alarm. Otherwise if TLD's page is the only one throwing errors, then proceed. Tired of these problems every other day:
    Sorry!
    
    An error was encountered while processing your request:
    
    There was an error communicating with the network. Please try again later.

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -5,30 +5,48 @@ import subprocess
 new_update = False
 base_date = datetime.datetime.utcfromtimestamp(1535662299) # datetime.datetime.now()
 
-def check_workshop_update(base_date):
+def retrieve_page_contents(url):
   try:
-    res = requests.get('https://steamcommunity.com/sharedfiles/filedetails/changelog/299974223?l=english')
+    res = requests.get(url)
   except requests.exceptions.RequestException as e:
     print(e)
     return False 
   soup = bs4.BeautifulSoup(res.text, features='html5lib')
+  return soup
 
+def get_page_title(soup):
   # swy: careful about error'ing out
   for title_tag in soup.select('head > title'):
-    title_text = str(title_tag.string)
+    return str(title_tag.string)
+  return ""
 
-    # swy: seems like Valve doesn't want to respect HTTP error codes, even on errors it throws 200 pages
-    #      take advantage of the title: "Steam Community :: Error", and maybe the body:
-    #      > Error > Sorry! > An error was encountered while processing your request:
-    #                       > That item does not exist. It may have been removed by the author.
-    if 'error' in title_text.lower():
-      print("[!] the page title is returning '%s', try to fix it on our end." % title_text)
+def check_workshop_update(base_date):
+  # swy: retrieve the main page from the changelog, grab another random Workshop page at the same time
+  #      to ensure it's not a general going-offline thing for the whole platform.
+  #      ignore that, we're looking for our own mod's problems.
+  tld_soup = retrieve_page_contents('https://steamcommunity.com/sharedfiles/filedetails/changelog/299974223?l=english')
+  swc_soup = retrieve_page_contents('https://steamcommunity.com/sharedfiles/filedetails/changelog/742671195?l=english')
+
+  tld_title_text = get_page_title(tld_soup)
+  swc_title_text = get_page_title(swc_soup)
+
+  # swy: seems like Valve doesn't want to respect HTTP error codes, even on errors it throws 200 pages
+  #      take advantage of the title: "Steam Community :: Error", and maybe the body:
+  #      > Error > Sorry! > An error was encountered while processing your request:
+  #                       > That item does not exist. It may have been removed by the author.
+  if 'error' in tld_title_text.lower():
+    # swy: generalized error? then just bail out
+    if 'error' in swc_title_text.lower():
+      print("[!] generalized downtime in the Steam Workshop platform, Valve messed up. Ignoring.")
+      return False
+    else:
+      print("[!] the page title is returning '%s', try to fix it on our end." % tld_title_text)
       subprocess.run(["handle-workshop-error"], shell=True)
       return False
 
   # swy: parse the page, find each potential changelog entry, and grab the latest date;
   #      as long as one of the entries is newer than the last time we checked we're done.
-  for anchor in soup.select('div#mainContents p[id]'):
+  for anchor in tld_soup.select('div#mainContents p[id]'):
     try:
       id_str  = anchor.get('id')
       id      = int(id_str)
