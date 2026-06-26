def9ed0af05f5027b9da788174f96a2a77bef290
Swyter <swyterzone@gmail.com>
2026-06-26 11:39:09 +0200

    Fix steamcommunity.com is seemingly now throwing 'HTTP/1.1 429 Too Many Requests' if we don't fake a user agent, sigh.
    titles: Steam Community :: Error Steam Community :: Error Steam Community :: L'Aigle :: Change Notes
    [!] generalized downtime in the Steam Workshop platform, Valve messed up. Ignoring.
    [e] making the scrapper sleep for 10 minutes.

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -9,7 +9,13 @@ def retrieve_page_contents(url):
   text = ''
   try:
     with requests.sessions.Session() as session:
-      res = session.get(url, timeout=5)
+      # swy: steamcommunity.com is seemingly now throwing 'HTTP/1.1 429 Too Many Requests' if we don't fake a user agent, sigh
+      res = session.get(url, timeout=5, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'es,en-US;q=0.9,en;q=0.8', 'DNT': '1', 'Sec-GPC': '1',
+        'Upgrade-Insecure-Requests': '1', 'Sec-Fetch-Dest': 'document', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1', 'Priority': 'u=0, i', 'Pragma': 'no-cache'
+      })
       text = res.text; res.close() # swy: this library is terribly designed and leaks HTTPS sessions: https://stackoverflow.com/a/45180470/674685
 
   except requests.exceptions.RequestException as e:
